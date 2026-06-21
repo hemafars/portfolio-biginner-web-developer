b7a8f7ec680fc032029358c96b1056b33da0fad7
hemafars <hemafars0@gmail.com>
2026-06-21 05:51:21 +0300

    ضبط القائمة المنسدلة لتناسب الجوال

---
+++ script.js
@@ -0,0 +1,8 @@
+// const link = document.getElementById("link");
+// function togglemenu() {
+//   if (link.style.display === "block") {
+//     link.style.display === "none";
+//   } else {
+//     link.style.display === "block";
+//   }
+// }
